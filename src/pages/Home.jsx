7ca43dd9874e9e6348992945b71348be4ac22e58
sergiorey23 <sergiorey@outlook.com>
2024-04-18 20:55:37 +0200

    Changes to be committed: 	new file:   src/pages/Home.css 	modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import './Home.css';
 
 const getIdFromUrl = (url) => url.substring(url.lastIndexOf('/', url.lastIndexOf('/') - 1) + 1, url.lastIndexOf('/'));
 
 const Home = () => {
     const [characters, setCharacters] = useState([]);
-    const [charactersImg, setCharactersImg] = useState([]);
+
     useEffect(() => {
         axios
             .get(`https://swapi.dev/api/people/`)
             .then((res) => {
-                setCharacters(res.data.results);
+                var results = res.data.results;
+                results = results.map((character) => {
+                    character.id = getIdFromUrl(character.url);
+                    return character;
+                });
+                setCharacters(results);
                 console.log(res.data)
             })
     }, [])
@@ -18,15 +24,17 @@ const Home = () => {
     return (
         <div>
             <h1>Star Wars Characters</h1>
-            <ul>
+            <div className="container">
                 {characters.map((character) => (
-                    <li key={character.name}>
+                    <div key={character.name}>
                         <a href={`/character/${character.url.split('/')[5]}`}>
                             {character.name}
                         </a>
-                    </li>
+                        <br />
+                        <img width="300px" src={`https://starwars-visualguide.com/assets/img/characters/${character.id}.jpg`} alt={character.name} />
+                    </div>
                 ))}
-            </ul>
+            </div>
         </div>
     )
 }
