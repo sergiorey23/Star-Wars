f816ec36fa9ed26ec01c02de344239711785cc57
sergiorey23 <sergiorey@outlook.com>
2024-04-18 23:35:11 +0200

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import './Home.css';
 
 const getIdFromUrl = (url) => url.substring(url.lastIndexOf('/', url.lastIndexOf('/') - 1) + 1, url.lastIndexOf('/'));
@@ -16,9 +17,11 @@ export const getPaginationLinks = (elementsAmount, elementsPerPage) => {
 const Home = () => {
     const [characters, setCharacters] = useState([]);
     const [paginationLinks, setPaginationLinks] = useState([]);
+    const query = new URLSearchParams(useLocation().search);
+    const page = query.get('page') || 1;
     useEffect(() => {
         axios
-            .get(`https://swapi.dev/api/people/`)
+            .get(`https://swapi.dev/api/people?page=${page}`)
             .then((res) => {
                 var results = res.data.results;
                 results = results.map((character) => {
@@ -26,7 +29,7 @@ const Home = () => {
                     return character;
                 });
                 setCharacters(results);
-                setPaginationLinks(getPaginationLinks(res.data.count, res.data.results.length));
+                setPaginationLinks(getPaginationLinks(res.data.count, 10));
             })
     }, [])
 
@@ -37,20 +40,22 @@ const Home = () => {
                 {characters.map((character) => (
                     <div key={character.name}>
                         <a href={`/character/${character.id}`} target="_blank">
+                            <img width="300px" src={`https://starwars-visualguide.com/assets/img/characters/${character.id}.jpg`} alt={character.name} />
+                            <br />
                             {character.name}
                             <br />
-                            <img width="300px" src={`https://starwars-visualguide.com/assets/img/characters/${character.id}.jpg`} alt={character.name} />
+                            <br />
                         </a>
                     </div>
                 ))}
             </div>
             <br />
             <div className="pagination">
-                {paginationLinks.map((page) => (
-                    <a key={page} href={`/page/${page}`}>{page + 1}</a>
+                {paginationLinks.map((link) => (
+                    <a style={{ margin: "10px" }} key={link} href={`?page=${link + 1}`}>{link + 1}</a>
                 ))}
             </div>
-        </div>
+        </div >
     )
 }
 
